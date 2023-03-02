1d1c66f36e86381028a92c9e9479b4684749fddf
Mohamed-Ali5555 <mohammedelbalshy4@gmail.com>
2023-03-03 01:49:17 +0200

    database integration

---
+++ app/Http/Controllers/VNewController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\v_new;
+use Illuminate\Http\Request;
+
+class VNewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\v_new  $v_new
+     * @return \Illuminate\Http\Response
+     */
+    public function show(v_new $v_new)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\v_new  $v_new
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(v_new $v_new)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\v_new  $v_new
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, v_new $v_new)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\v_new  $v_new
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(v_new $v_new)
+    {
+        //
+    }
+}
