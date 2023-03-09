e40987055c48cf4a995e5fd98373b4c75f3f3eec
Mohamed-Ali5555 <mohammedelbalshy4@gmail.com>
2023-03-09 21:11:43 +0200

    video generate

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -50,6 +50,34 @@ class VideoController extends Controller
     
    }
 
+   public function update(Request $request,  $id)
+   {
+    $this->validate($request, [
+        'title' => 'required|string|max:255',
+        'video' => 'required|file|mimetypes:video/mp4',
+        ]);
+
+        $Video = Video::findOrFail($id);
+
+     
+       
+
+        if ($request->hasFile('video'))
+        {
+            Storage::delete($Video->video);
+
+            $path = $request->file('video')->store('videos', ['disk' =>'my_files']);
+            $video->video = $path;
+
+        }
+        $versionMes->update([
+            'title'=>$request->title,
+            'video'=>$path,
+        ]);
+
+ return redirect()->route('video.index')->with('success','videos has been created successfully.');
+
+   }
 
    public function destroy( $id)
    {
@@ -96,21 +124,25 @@ class VideoController extends Controller
     //     'title' => 'required|string',
     //     'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
     // ]);
-    $title = $request->title;
-    $desc = $request->desc;
+ 
     // $image = $request->image;
 
 
-    $data = $request->validate([
-        'title' => 'required|string',
-        'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-    ]);
+    // $data = $request->validate([
+    //     'title' => 'required|string',
+    //     'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+    // ]);
 
-  $data['image'] = Storage::putFile("news",$data['image']);
+//   $data['image'] = Storage::putFile("news",$data['image']);
    
   
 //   return $request->all();
 // $image2 =  Storage::putFile("news",$image);
+
+
+// $data=$request->all();
+
+
 
 // $imageNew = '';
 // if($request->hasFile('image')){
@@ -120,6 +152,26 @@ class VideoController extends Controller
 
 // }
 
+
+// $path = $request->file('image');
+// $image = $path->getClientOriginalName();
+// $path->move(public_path('images/image'), $image);
+
+
+$image=array();
+if($files=$request->file('image')){
+    foreach($files as $file){
+        $name=$file->getClientOriginalName();
+        $file->move(public_path('backend/assets/uploads'),$name);
+        $image[]=$name;
+
+    }
+
+}
+
+
+$title = $request->title;
+ $desc = $request->desc;
 // $image =  Storage::putFile("news",$request->image);
 
 // Storage::disk('news')->put('file.jpg', file_get_contents($photo));
@@ -127,7 +179,12 @@ class VideoController extends Controller
 // return $request->all();
 
 
-    // $data['image'] = Storage::putFile("news",$data['image']);
+// $path = Storage::putFile('news', $request->file('image'));
+// $disk = Storage::disk('news');
+// Storage::disk('local')->put($path . '/' . $originalName, $request->file);
+
+    // $data['image'] = \Storage::putFile("news",$data['image']);
+
 // $image =  Storage::putFile("news",$request->image);
 //  return $image;
     for($i=0; $i < count($desc);$i++){
@@ -136,13 +193,35 @@ class VideoController extends Controller
             'video_id'  =>$id,
             'title'  =>$title[$i],
             'desc'  =>$desc[$i],
-            'image'  =>$data['image'] [$i],
+            // 'image'  =>$imageNew [$i],
+           
+            'image' => $image[$i],
+            // 'image' => json_encode($image)
+
+
 
         ];
  
         $datasave1=v_new::create($datasave);
     }
 
+
+  
+            
+            
+    // foreach($data['desc'] as $key=>$val){
+    //     if(!empty($val)){
+    //         $attribute=new v_new;
+    //         // $attribute['original_price']=$val;
+    //         $attribute['title']=$data['title'][$key];
+    //         $attribute['desc']=$data['desc'][$key];
+    //         $attribute['video_id']=$id;
+    //         // $attribute['image']=$data['image'][$key];
+    //         $attribute['image']=$imageNew[$val];
+
+    //         $attribute->save();
+    //     }
+    // }
 
             return redirect()->back()->with('success','product attribute successfuly add');
 
