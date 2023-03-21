c5d6453c6638737ee3582589f3015b966e3186ef
Eng-Ahmed-hub <Eng-Ahmed-hub>
2023-03-21 18:59:55 +0200

    update company

--- resources/views/frontend/company_details.blade.php
+++ resources/views/frontend/company_details.blade.php
@@ -2,30 +2,60 @@
 @section('content')
 
 <div class="bg_full_cp">
-	<div class="bg_full" style="background-image: url({{ asset("storage/$detail->image") }}); background-repeat: no-repeat; background-position: center center;height:500px;">
+	<div class="bg_full" style="background-image: url({{ asset("storage/$detail->image") }}); background-repeat: no-repeat; background-position: center center;">
 		<div class="container">
 			<div class="ct">
 				<h2 class="page_title">
-					<span>Careers Page</span>
 				</h2>
 				<div class="breadcrumb-links">
-					<a href="../../index.html">Home</a>
+					<a href="{{ route('index') }}">Home</a>
 					<span> / Careers Page</span>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-<div class="cosl col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="row">
-        <div class="image-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <img src="../../images/careers1.jpg" alt="">
-        </div>
-        <div class="content-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-        </div>
-    </div>
+<div class="careers">
+	<div class="container">
+		<div class="title">
+			<h3>Careers</h3>
+		</div>
+		{{-- <div class="row">
+			<div class="cosl col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="row">
+					<div class="image-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<img src="{{ asset("storage/$detail->banner_img") }}" alt="">
+					</div>
+					<div class="content-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+                        <h4>{{ $detail->title }}</h4>
+						<td>{!!html_entity_decode($detail->desc)!!}</td>
+					</div>
+				</div>
+			</div> --}}
+			<div class="cosl col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="row">
+					<div class="content-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<h4>{{ $detail->title }}</h4>
+						<td>{!!html_entity_decode($detail->desc)!!}</td>
+					</div>
+					<div class="image-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						{{-- <img src="{{ asset("storage/$detail->banner_img") }}" alt=""> --}}
+					</div>
+				</div>
+			</div>
+			{{-- <div class="cosl col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="row">
+					<div class="image-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						{{-- <img src="{{ asset("storage/$detail->banner_img") }}" alt=""> --}}
+					{{-- </div>
+					<div class="content-block col-lg-6 col-md-6 col-sm-12 col-xs-12">
+						<h4>{{ $detail->title }}</h4>
+						<td>{!!html_entity_decode($detail->desc)!!}</td>
+					</div>
+				</div> --}}
+			{{-- </div> --}}
+		</div>
+	</div>
 </div>
 
 @endsection
